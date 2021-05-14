e6c8d0cd3d9ca5e5ff9fbae9b98877f80eafa0e7
Andy Bryson <abryson@redhat.com>
2021-05-13 21:04:58 -0400

    added @Value import

--- opentelemetry-app-a/src/main/java/com/javastaff/tracing/opentelemetrya/Service1.java
+++ opentelemetry-app-a/src/main/java/com/javastaff/tracing/opentelemetrya/Service1.java
@@ -3,6 +3,7 @@ package com.javastaff.tracing.opentelemetrya;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
--- opentracing-app-a/src/main/java/com/javastaff/tracing/opentracinga/AppAController.java
+++ opentracing-app-a/src/main/java/com/javastaff/tracing/opentracinga/AppAController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
